952f54a745173a8298298350b7a339e4554e4b27
NeriMilan <hdjt940280374@gmail.com>
2021-11-22 15:39:49 +0800

    商品管理v1

---
+++ src/main/java/com/shangma/mapper/goods/GoodsTypeMapper.java
@@ -0,0 +1,8 @@
+package com.shangma.mapper.goods;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.shangma.entity.goods.GoodsType;
+
+public interface GoodsTypeMapper extends BaseMapper<GoodsType> {
+
+}
